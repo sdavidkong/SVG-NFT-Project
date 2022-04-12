44200e403f0702e56609c77ba4cd3da815e8ad2e
S David Kong <76503030+sdavidkong@users.noreply.github.com>
2022-04-12 21:18:45 +0300

    Add files via upload

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,94 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+import { Base64 } from "./libraries/Base64.sol";
+
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] firstWords = ["bitcoin ", "ether ", "link ", "ripple ", "cardano ", "thegraph "];
+    string[] secondWords = ["belize ", "austria ", "singapore ", "iceland ", "norway ", "senegal "];
+    string[] thirdWords = ["burrito ", "pizza ", "padthai ", "queso ", "cheesecake ", "otoro "];
+
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract!");
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        uint rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        uint rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        uint rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+        require(newItemId <= 49);
+
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+        string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+        string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                     '{"name": "',
+                     combinedWord,
+                     '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                     Base64.encode(bytes(finalSvg)),
+                     '"}'   
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(string(        
+            abi.encodePacked(
+            "https://nftpreview.0xdev.codes/?code=",
+            finalTokenUri)));
+        console.log("--------------------\n");
+
+        
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId,finalTokenUri);
+        console.log("An NFT w/ ID %s has been minted by %s", newItemId, msg.sender);
+        _tokenIds.increment();
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
+    }
+
+    function getTotalNFTsMintedSoFar() public view returns(uint256) {
+       return _tokenIds.current();
+    }
+}
